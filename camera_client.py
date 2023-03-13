f05b1e6f69e4e321697ffad181c3497f75f3eab4
kokolem <vitekp@pm.me>
2023-03-13 18:21:00 +0100

    Rewrite camera client to never drop keyframes

--- camera_client.py
+++ camera_client.py
@@ -1,12 +1,11 @@
 import asyncio
-import io
 from threading import Condition
 
 import websockets as websockets
 from nacl.public import PrivateKey, Box, PublicKey
 from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder, Quality
-from picamera2.outputs import FileOutput
+from picamera2.outputs import Output
 
 with open("receiver_public.key", "rb") as receiver_public_key_file:
     receiver_public_key = PublicKey(receiver_public_key_file.read())
@@ -17,14 +16,20 @@ with open("camera_private.key", "rb") as camera_private_key_file:
 encryptor = Box(camera_private_key, receiver_public_key)
 
 
-class CameraOutput(io.BufferedIOBase):
+class AsyncIterableOutput(Output):
     def __init__(self):
-        self.frame = None
+        super().__init__()
+        self.latest_non_keyframe = None
+        self.latest_keyframe = None
         self.new_frame_condition = Condition()
 
-    def write(self, buf: bytes):
+    def outputframe(self, frame, keyframe=True, timestamp=None):
         with self.new_frame_condition:
-            self.frame = encryptor.encrypt(buf)
+            encrypted_frame = encryptor.encrypt(frame)
+            if keyframe:
+                self.latest_keyframe = encrypted_frame
+            else:
+                self.latest_non_keyframe = encrypted_frame
             self.new_frame_condition.notify_all()
 
     def __aiter__(self):
@@ -35,16 +40,29 @@ class CameraOutput(io.BufferedIOBase):
 
     def wait_for_new_frame(self):
         with self.new_frame_condition:
+            preferred_frame = self.get_preferred_frame()
+            if preferred_frame:
+                return preferred_frame
             self.new_frame_condition.wait()
-            frame = self.frame
-        return frame
+            return self.get_preferred_frame()
+
+    def get_preferred_frame(self):
+        preferred_frame = None
+        if self.latest_keyframe:
+            preferred_frame = self.latest_keyframe
+            self.latest_keyframe = None
+        elif self.latest_non_keyframe:
+            preferred_frame = self.latest_non_keyframe
+            self.latest_non_keyframe = None
+        return preferred_frame
 
 
 picam2 = Picamera2()
 picam2.configure(picam2.create_video_configuration(main={"size": (1296, 972)}))
-camera_output = CameraOutput()
+camera_output = AsyncIterableOutput()
 encoder = H264Encoder()
-picam2.start_recording(encoder, FileOutput(camera_output), Quality.VERY_HIGH)
+picam2.start_encoder(encoder, camera_output, quality=Quality.VERY_HIGH)
+picam2.start()
 
 
 async def send_frames():
