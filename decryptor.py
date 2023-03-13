27576dacb38559257b95d5f520951d798e0a3aea
kokolem <vitekp@pm.me>
2023-03-13 21:18:07 +0100

    Implement output file rotation

--- decryptor.py
+++ decryptor.py
@@ -1,3 +1,5 @@
+import sys
+
 from nacl.public import PrivateKey, Box, PublicKey
 
 with open("camera_public.key", "rb") as camera_public_key_file:
@@ -8,8 +10,8 @@ with open("receiver_private.key", "rb") as receiver_private_key_file:
 
 decryptor = Box(receiver_private_key, camera_public_key)
 
-with open("output.h264.enc", "rb") as encrypted_file:
-    with open("output.h264", "wb") as decrypted_file:
+with open(sys.argv[1], "rb") as encrypted_file:
+    with open(sys.argv[1][:-4], "wb") as decrypted_file:
         for encrypted_frame in encrypted_file.read().split(b"--FRAME"):
             decrypted_frame = decryptor.decrypt(encrypted_frame)
             decrypted_file.write(decrypted_frame)
--- receiver_server.py
+++ receiver_server.py
@@ -1,13 +1,26 @@
 import asyncio
+import datetime
+
 import websockets
+
+max_file_size = 50 * 1_000_000
 
 
 async def save_frame(websocket):
-    with open("output.h264.enc", "wb") as output_file:
-        async for frame in websocket:
-            output_file.write(frame)
-            output_file.write(b"--FRAME")
-            output_file.flush()
+    current_file_size = 0
+    current_file = open(f"{datetime.datetime.now().isoformat(timespec='seconds')}.h264.enc", "wb")
+
+    async for frame in websocket:
+        if current_file_size + len(frame) > max_file_size:
+            current_file.close()
+            current_file = open(f"{datetime.datetime.now().isoformat(timespec='seconds')}.h264.enc", "wb")
+            current_file_size = 0
+
+        current_file.write(frame)
+        current_file.write(b"--FRAME")
+        current_file.flush()
+
+        current_file_size += len(frame)
 
 
 async def run_server():
