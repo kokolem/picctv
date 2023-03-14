cdd70cead961da59280bfebb0c85a72da5efd85d
kokolem <vitekp@pm.me>
2023-03-15 00:52:40 +0100

    Save keys in hex encoding

--- utils/generate_keys.py
+++ utils/generate_keys.py
@@ -1,3 +1,4 @@
+from nacl.encoding import HexEncoder
 from nacl.public import PrivateKey
 
 camera_private_key = PrivateKey.generate()
@@ -6,13 +7,13 @@ receiver_private_key = PrivateKey.generate()
 receiver_public_key = receiver_private_key.public_key
 
 with open("receiver_public.key", "wb") as receiver_public_key_file:
-    receiver_public_key_file.write(receiver_public_key.encode())
+    receiver_public_key_file.write(receiver_public_key.encode(HexEncoder))
 
 with open("receiver_private.key", "wb") as receiver_private_key_file:
-    receiver_private_key_file.write(receiver_private_key.encode())
+    receiver_private_key_file.write(receiver_private_key.encode(HexEncoder))
 
 with open("camera_public.key", "wb") as camera_public_key_file:
-    camera_public_key_file.write(camera_public_key.encode())
+    camera_public_key_file.write(camera_public_key.encode(HexEncoder))
 
 with open("camera_private.key", "wb") as camera_private_key_file:
-    camera_private_key_file.write(camera_private_key.encode())
+    camera_private_key_file.write(camera_private_key.encode(HexEncoder))
